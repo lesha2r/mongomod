c908eda254547830b44a857175fbf23696fcf27f
lesha2r <leshatour@gmail.com>
2025-09-20 15:39:12 +0300

    test: 1

--- src/MongoModel/methods/set.ts
+++ src/MongoModel/methods/set.ts
@@ -3,7 +3,7 @@ import MongoModel from '../MongoModel.js';
 function set(this: MongoModel, data: Record<string, any>): MongoModel {
     if (!this.modelData) this.modelData = {};
     this._modelDataBeforeSave = this.data(true);
-
+    console.log('1')
     // Handle undefined values by deleting keys from modelData
     const updatedModelData = { ...this.modelData };
     
